5d9be33839be57bbcb5cd8bc1cef6a4394c45af5
YazonDA <yazonda.29@gmail.com>
2020-11-21 20:58:08 +0300

    added to algorithm new task for the future

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import sys
 import m_checker.modules as do_it
 
-
 def main():
 	_config = do_it.config('settings.ini')
 	_ts = do_it.config('/home/yda/Documents/ts.ini')
 	tst_mote = do_it.Mote(_config, _ts)
 
 	#print(tst_mote.format_dp)
+	#print(_ts['TS']['log'])
 
 	return 0
 
